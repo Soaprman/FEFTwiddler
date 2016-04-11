c718cfa625bdcb6e244e90fd77a3ab1211939cc5
Douman <douman@gmx.se>
2016-04-11 10:42:06 +0300

    Fallback to default dir on file open

--- FEFTwiddler/Form1.Designer.cs
+++ FEFTwiddler/Form1.Designer.cs
@@ -297,7 +297,7 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileName = "";
             // 
             // tabControl1
             // 
