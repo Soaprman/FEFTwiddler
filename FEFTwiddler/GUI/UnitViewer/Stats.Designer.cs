4df4bf6f44ccf150929742abf39223de38668a8e
Soaprman <m.grieshober@gmail.com>
2016-04-17 18:12:17 -0400

    Change stats label

--- FEFTwiddler/GUI/UnitViewer/Stats.Designer.cs
+++ FEFTwiddler/GUI/UnitViewer/Stats.Designer.cs
@@ -50,9 +50,9 @@
             this.label14.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.label14.Location = new System.Drawing.Point(1, 2);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(57, 13);
+            this.label14.Size = new System.Drawing.Size(64, 13);
             this.label14.TabIndex = 62;
-            this.label14.Text = "Stat bytes:";
+            this.label14.Text = "Stats (WIP):";
             // 
             // txtStatBytes
             // 
