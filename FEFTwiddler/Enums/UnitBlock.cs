dbee7ac35d4e8ff83db2326fd72b1caae509871d
Soaprman <m.grieshober@gmail.com>
2016-04-30 11:09:38 -0400

    Add a comment about code that isn't there. Wow!

--- FEFTwiddler/Enums/UnitBlock.cs
+++ FEFTwiddler/Enums/UnitBlock.cs
@@ -3,6 +3,7 @@
     public enum UnitBlock : byte
     {
         Deployed = 0x00,
+        // There might be "allied" and "enemy" blocks in Map saves.
         Living = 0x03,
         DeadByGameplay = 0x04,
         Absent = 0x05,
