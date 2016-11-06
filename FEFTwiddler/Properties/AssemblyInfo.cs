04796d4b75de4292224e5eaa218ad055c8cacf05
Soaprman <m.grieshober@gmail.com>
2016-11-06 09:29:01 -0500

    Change assembly info

--- FEFTwiddler/Properties/AssemblyInfo.cs
+++ FEFTwiddler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FEFManager")]
+[assembly: AssemblyTitle("FEFTwiddler: Fire Emblem Fates Save Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FEFManager")]
+[assembly: AssemblyProduct("FEFTwiddler")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
